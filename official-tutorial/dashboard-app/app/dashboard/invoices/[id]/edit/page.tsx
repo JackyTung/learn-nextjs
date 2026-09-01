8e9cad757caef1a2db7e2d43e45807b0b75e0a3a
JackyTung <jackytung8085@gmail.com>
2026-09-01 22:43:54 +0800

    dashboard: ch12 error handle

--- official-tutorial/dashboard-app/app/dashboard/invoices/[id]/edit/page.tsx
+++ official-tutorial/dashboard-app/app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,7 +1,7 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
-
+import { notFound } from 'next/navigation';
 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
     const params = await props.params;
@@ -11,7 +11,7 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
         fetchCustomers(),
     ]);
     if (!invoice) {
-        return <div>Invoice not found</div>;
+        notFound();
     }
     return (
         <main>
